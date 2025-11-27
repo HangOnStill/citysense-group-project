14fc342dd21eb4b679c963bada5c93a644c5b0c4
Luca Greene <sglgree4@liverpool.ac.uk>
2025-11-27 15:31:26 -0500

    SImulator can now run step by step/in batches (i.e. 1 hour at a time)
    Can still do day/month/year at once

--- src/sim/Simulator.hpp
+++ src/sim/Simulator.hpp
@@ -3,11 +3,14 @@
 #include <vector>
 #include <string>
 #include <chrono>
+#include <fstream>
+#include <sstream>
+
 
 #include "Clock.hpp"
 #include "SeededRNG.hpp"
 #include "SimulatorProfile.hpp"
-
+#include "Checkpoint.hpp"
 #include "../model/SensorRecord.hpp"
 
 namespace sim{
@@ -15,8 +18,23 @@ class Simulator{
     public:
         Simulator(Clock clock, SeededRNG rng);
 
+        void start(SimulatorProfile profile);
+        void pause();
+        void resume();
+        bool isrunning() const {return running_;}
+
+        std::vector<model::SensorRecord> next_step();
+        std::vector<model::SensorRecord> next_batch(int steps);
+
+        void save_state(const std::string& path) const;
+        void load_state(const std::string& path);
+
         //Generates one full day of data
         std::vector<model::SensorRecord> generate_day(SimulatorProfile profile);
+        //Generates one full day of data
+        std::vector<model::SensorRecord> generate_month(SimulatorProfile profile, int month);
+        //Generates one full year of data
+        std::vector<model::SensorRecord> generate_year(SimulatorProfile profile);
     
     private:
         Clock clock_;
@@ -24,6 +42,12 @@ class Simulator{
 
         double last_noise_db_ = 55.0;
         double last_pm25_     = 20.0;
+
+        bool running_ = false;
+        SimulatorProfile profile_{SimulatorProfile::Weekday};
+
+        std::vector<model::SensorRecord> generate_step(SimulatorProfile profile);
+
 
         //Helpers to simulate records
         model::SensorRecord generate_air_record(std::chrono::system_clock::time_point ts, int zone_id, const std::string& sensor_id, 
