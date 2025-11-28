eb19c06774e080ecbe6137ef9421619f71eb91f1
Shane McAlister-Colacio <smcal087@uottawa.ca>
2025-11-28 08:31:55 -0500

    NoiseSpike detector in progress

--- src/detectors/NoiseSpike.hpp
+++ src/detectors/NoiseSpike.hpp
@@ -1,5 +1,7 @@
 #pragma once
 #include "../core/Detector.hpp"
+#include "../core/Window.hpp"
+#include "../core/Finding.hpp"
 
 namespace detectors {
 
@@ -11,9 +13,22 @@ public:
     NoiseSpike(double db_thr, int count_thr, int window_min)
       : db_thr_(db_thr), count_thr_(count_thr), window_min_(window_min) {}
 
-    void detect(const core::Window& /*w*/) override {
+    std::vector<core::Finding> detect(const core::Window& w) override {
         // TODO: db threshold exceeded M times in T minutes.
-        (void)db_thr_; (void)count_thr_; (void)window_min_;
+        //(void)db_thr_; (void)count_thr_; (void)window_min_;
+        core::Window timed_win = w;
+        const auto max_ts = timed_win.records.back().ts;
+        const auto cutoff = max_ts - std::chrono::minutes(window_min_);
+
+        // NOTE: We are only considering records in the last window_min_ minutes
+        auto erase_it = std::remove_if(
+            timed_win.records.begin(),
+            timed_win.records.end(),
+        [&](const model::SensorRecord& rec){ return rec.ts < cutoff; });
+
+        timed_win.records.erase(erase_it, timed_win.records.end());
+
+        
     }
 };
 
