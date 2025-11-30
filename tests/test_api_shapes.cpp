8720287951ec6bc09662d9b8059f2d37c5e4efa2
Shane McAlister-Colacio <2879251M@student.gla.ac.uk>
2025-11-29 20:33:44 -0500

    removed test 1.0 thing

--- tests/test_api_shapes.cpp
+++ tests/test_api_shapes.cpp
@@ -33,5 +33,5 @@ TEST_CASE("Public API surfaces exist (types, methods)") {
 
     io::ReaderCSV reader({ "data/air.csv" });
     auto batch = reader.next_batch(10);
-    //REQUIRE((batch.size() > 0 || batch.empty()));
+    REQUIRE((batch.size() > 0 || batch.empty()));
 }
